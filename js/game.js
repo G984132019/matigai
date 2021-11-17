28153afecb3f6b8f51684821517cb844435f21e1
G984132019 <71371311+G984132019@users.noreply.github.com>
2021-11-17 18:15:44 +0900

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,6 +1,15 @@
 let timer = null;
 const max = 3;
 let count = 0;
+let eTime;
+
+const APPLICATION_KEY = "e0bd460ddaa733fb60fa17d76805eb473c049abcb4e95b57208d408034dce139";
+const CLIENT_KEY = "4269fe883a87cfb7bcad851004edca12ffdbd3fb4c03234ede73d36fb3d30da8";
+const ncmb = new NCMB(APPLICATION_KEY,CLIENT_KEY);
+const DBName = "GameScore";
+
+let GameScore = ncmb.DataStore(DBName);
+
 function init() {
   if (timer == null) {
     start = new Date();
@@ -16,7 +25,7 @@ function gameStart() {
     let s = document.createElement("span");
     s.textContent = q[qNum][0];
     s.setAttribute("id", "num"+i);
-    
+
     s.addEventListener('click',function () {
       if(this.textContent == q[qNum][1]){
         correct.play();
@@ -27,7 +36,9 @@ function gameStart() {
         if(count<max){
           gameStart();
         }else{
-          alert("クリア");
+
+          save();
+          load();
           clearTimeout(timer);
         }
       }else{
@@ -49,7 +60,36 @@ function gameStart() {
 
 function time() {
   let now = new Date();
-  let eTime = parseInt((now.getTime() - start.getTime())/1000);
+  eTime = parseInt((now.getTime() - start.getTime())/1000);
   score.textContent = eTime;
   timer = setTimeout("time()",1000);
+}
+
+function save(){
+  let test = new GameScore();
+  let key = "score";
+  let value = eTime;
+  test.set(key, parseInt(value));
+  test.save()
+  .then(function(){
+    console.log("成功");
+  })
+  .catch(function(err){
+    console.log("エラー発生：" + err);
+  });
+}
+function load(){
+  GameScore
+  .order("score")
+  .fetchAll()
+  .then(function(results){
+    if(eTime<results[0].score){
+      alert("ハイスコア");
+    }else{
+      alert("クリア");
+    }
+  })
+  .catch(function(err){
+    console.log("エラー発生：" + err);
+  });
 }
